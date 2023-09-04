e271c6a498f709bbbb9f14a25f275b34cffb731f
Pinta365 <hello@pinta.land>
2023-09-04 19:47:44 +0200

    this special string type was causing problems

--- src/types/oura.ts
+++ src/types/oura.ts
@@ -5,13 +5,8 @@
  * @author Pinta <https://github.com/Pinta365>
  * @license MIT
  */
-type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type zeroToNine = 0 | oneToNine;
-type YYYY = `19${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`;
-type MM = `0${oneToNine}` | `1${0 | 1 | 2}`;
-type DD = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`;
 
-export type DateFormat = `${YYYY}-${MM}-${DD}`;
+export type DateFormat = string;
 
 export interface PersonalInfo {
     id: string;
